81f16fe0760e053e5d3b8a75ab2261dffaa9807e
DhaleemaDavis <108056388+DhaleemaDavis@users.noreply.github.com>
2024-07-31 13:31:37 +0200

    Update Sql_select statement.sql

--- Sql_select statement.sql
+++ Sql_select statement.sql
@@ -1,39 +1,39 @@
 # SELECT Statement
 
 
-select*
-from parks_and_recreation.employee_demographics;
+SELECT *
+FROM parks_and_recreation.employee_demographics;
 
-select*
-from parks_and_recreation.employee_salary;
+SELECT *
+FROM parks_and_recreation.employee_salary;
 
-select first_name
-from parks_and_recreation.employee_demographics;
+SELECT first_name
+FROM parks_and_recreation.employee_demographics;
 
-select first_name, last_name,age
-from parks_and_recreation.employee_demographics;
+SELECT first_name, last_name,age
+FROM parks_and_recreation.employee_demographics;
 
-select first_name,
+SELECT first_name,
 last_name,
 age
-from parks_and_recreation.employee_demographics;
+FROM parks_and_recreation.employee_demographics;
 
-select first_name,
+SELECT first_name,
 last_name,
 age,
 age+10,
 (age + 10) * 10 /2
-from parks_and_recreation.employee_demographics;
+FROM parks_and_recreation.employee_demographics;
 
 
-select distinct first_name,
+SELECT DISTINCT first_name,
 age
-from parks_and_recreation.employee_demographics;
+FROM parks_and_recreation.employee_demographics;
 
 
-select distinct gender
-from parks_and_recreation.employee_demographics;
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
 
-select distinct gender,
+SELECT DISTINCT gender,
 age
-from parks_and_recreation.employee_demographics;
+FROM parks_and_recreation.employee_demographics;
