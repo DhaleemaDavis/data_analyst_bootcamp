76970bc514b9c0d9536cbf033c845602b1df7b66
DhaleemaDavis <108056388+DhaleemaDavis@users.noreply.github.com>
2024-08-02 12:39:28 +0200

    Add files via upload

--- Sql_subqueries.sql
+++ Sql_subqueries.sql
@@ -5,7 +5,7 @@ from parks_and_recreation.employee_demographics;
 select *
 from parks_and_recreation.employee_salary;
 
-
+# subquery using where clause
 # printing the information of employees in dept. 1
 select *
 from parks_and_recreation.employee_demographics
@@ -14,3 +14,70 @@ where employee_id in
                 from parks_and_recreation.employee_salary
                 where dept_id = 1)
 ;
+
+
+# using select statement
+# comparing the individual salary with the average salary
+
+
+select first_name, salary, avg(salary)
+from parks_and_recreation.employee_salary
+group by first_name, last_name, salary
+;
+
+select first_name, salary,
+(select avg(salary)
+from parks_and_recreation.employee_salary)   # for getting avg salary
+from parks_and_recreation.employee_salary;  
+
+
+# subquerying in from statement
+
+select gender, avg(age), min(age), max(age), count(age)
+from parks_and_recreation.employee_demographics
+group by gender;
+
+select gender, avg(`max(age)`)
+from
+(select gender, avg(age), 
+min(age) min_age,
+max(age), 
+count(age)
+from parks_and_recreation.employee_demographics
+group by gender) as agg_table
+group by gender;
+
+select  avg(`max(age)`)
+from
+(select gender, avg(age), 
+min(age) min_age,
+max(age), 
+count(age)
+from parks_and_recreation.employee_demographics
+group by gender) as agg_table
+;
+
+
+
+
+
+
+
+
+
+
+SELECT *
+FROM (SELECT gender, MIN(age), MAX(age), COUNT(age),AVG(age)
+FROM employee_demographics
+GROUP BY gender) ;
+
+
+SELECT gender, AVG(Min_age)
+FROM (SELECT gender, 
+MIN(age) Min_age, 
+MAX(age) Max_age, 
+COUNT(age) Count_age ,
+AVG(age) Avg_age
+FROM employee_demographics
+GROUP BY gender) AS Agg_Table
+GROUP BY gender;
